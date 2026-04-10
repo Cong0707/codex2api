b58534f0cb139408e105b1ebb39197752eb9a54a
Your Name <you@example.com>
2026-04-10 12:31:28 +0000

    Make request body size limit configurable

--- config/config_test.go
+++ config/config_test.go
@@ -182,7 +182,6 @@ func TestLoadReadsMaxRequestBodySizeFromEnv(t *testing.T) {
 		t.Fatalf("MaxRequestBodySize = %d, want %d", got, 64*1024*1024)
 	}
 }
-
 func TestLoadReadsRedisTLSSettings(t *testing.T) {
 	keys := []string{
 		"CODEX_PORT",
